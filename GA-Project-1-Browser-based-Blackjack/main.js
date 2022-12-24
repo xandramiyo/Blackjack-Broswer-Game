721783160bb87b28eba62e1c487bf4feedcadb5f
xandramiyo <aj.sumiyoshi@gmail.com>
2022-12-23 17:32:05 -0800

    added function to shuffle deck + skeleton for drawing the player and dealer hands

--- GA-Project-1-Browser-based-Blackjack/main.js
+++ GA-Project-1-Browser-based-Blackjack/main.js
@@ -1,20 +1,62 @@
+
+/*----- constants -----*/
+
 const suits = ['d', 'h', 'c', 's']
 const faces = ['A', 'K', 'Q', 'J', '10', '09', '08', '07', '06', '05', '04', '03', '02']
 const deck = []
 
+const PLAYERS = {
+    '1': {
+        name: 'Player',
+    },  
+
+    '-1': {
+        name: 'Dealer',
+    },
+}
+
+/*----- app's state (variables) -----*/
+
+let turn, winner, blackjack, playerHand, dealerHand
+
+//stay?
+
+/*----- cached element references -----*/
+const hitBtn = document.getElementById('hit')
+const stayBtn = document.getElementById('stay')
+const splitBtn = document.getElementById('split')
+
+
+/*----- event listeners -----*/
+ 
+hitBtn.addEventListener('click', hit)
+stayBtn.addEventListener('click', stay)
+splitBtn.addEventListener('click',split)
+
+
+/*----- functions -----*/
+
 function generateDeck() {
     suits.forEach(suit => {
         faces.forEach(face => {
-            // let sum = if (face === 'K' || 'Q' || 'J') {
-            //     score = 10
-            // }
             deck.push({ 
                 'face': suit + face
-                // VALUE SHOULD EQUAL SCORE TO DETERMINE BUST OR NOT
-                // 'value': null;
             })
         })
     })
+}
+
+function shuffleDeck() {
+    for(let i = 0; i < deck.length; i++) {
+        let j = Math.floor(Math.random() * deck.length);
+        deck[i] = deck[j];
+    }
+}
+
+function drawHands() {
+    for(let i = 0; i < 2; i++) {
+        
+    }
 }
 
 //This will generate a full deck of 52
@@ -22,6 +64,18 @@ function renderDeck() {
     deck.forEach(card => {
         const cardEl = document.createElement('div')
         cardEl.className = 'card ' + card.face
-        document.querySelector('body').append(cardEl)
+        document.querySelector('deck').append(cardEl)
     })
 }
+
+// function hit() {
+
+// }
+
+// function stay() {
+
+// }
+
+// function split() {
+
+// }
