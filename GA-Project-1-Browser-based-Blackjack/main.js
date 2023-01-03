2208eedd31a0594bfdf9bbee306e590d671f3a0d
xandramiyo <aj.sumiyoshi@gmail.com>
2023-01-03 13:29:10 -0800

    added hit and split functionality + changed css styling(not done)

--- GA-Project-1-Browser-based-Blackjack/main.js
+++ GA-Project-1-Browser-based-Blackjack/main.js
@@ -19,7 +19,6 @@ const PLAYERS = {
 
 let turn, winner, blackjack, playerHand, dealerHand
 
-//stay?
 
 /*----- cached element references -----*/
 const hitBtn = document.getElementById('hit')
@@ -36,6 +35,13 @@ splitBtn.addEventListener('click',split)
 
 /*----- functions -----*/
 
+function init() {
+    turn = 1
+    generateDeck()
+    shuffleDeck()
+    drawHands()
+}
+
 function generateDeck() {
     suits.forEach(suit => {
         faces.forEach(face => {
@@ -49,33 +55,98 @@ function generateDeck() {
 function shuffleDeck() {
     for(let i = 0; i < deck.length; i++) {
         let j = Math.floor(Math.random() * deck.length);
+        let temp = deck[i];
         deck[i] = deck[j];
+        deck[j] = temp;
     }
 }
 
 function drawHands() {
-    for(let i = 0; i < 2; i++) {
-        
+    playerHand = deck.splice(0, 2)
+    dealerHand = deck.splice(0, 2)
+    if (sumHand(playerHand) >= 21) {
+        hitBtn.setAttribute("disabled", "")
+        stayBtn.setAttribute("disabled", "")
+        splitBtn.setAttribute("disabled", "")
+        changeTurn()
     }
 }
 
 //This will generate a full deck of 52
-function renderDeck() {
-    deck.forEach(card => {
+// function renderDeck() {
+//     deck.forEach(card => {
+//         const cardEl = document.createElement('div')
+//         cardEl.className = 'card ' + card.face
+//         document.querySelector('body').append(cardEl)
+//     })
+// }
+
+function renderHands() {
+    document.querySelector('.playerHand').innerHTML = " "
+    playerHand.forEach(card => {
         const cardEl = document.createElement('div')
         cardEl.className = 'card ' + card.face
-        document.querySelector('deck').append(cardEl)
-    })
+        document.querySelector('.playerHand').append(cardEl)
+})}
+
+function hit() {
+    playerHand.push(deck.shift())
+    if (sumHand(playerHand) >= 21) {
+        hitBtn.setAttribute("disabled", "")
+        stayBtn.setAttribute("disabled", "")
+        splitBtn.setAttribute("disabled", "")
+        changeTurn()
+    }
 }
 
-// function hit() {
-
-// }
-
-// function stay() {
-
-// }
+function stay() {
+    changeTurn()
+    stayBtn.setAttribute("disabled", "")
+    hitBtn.setAttribute("disabled", "")
+    splitBtn.setAttribute("disabled", "")
+}
 
 // function split() {
 
 // }
+
+function sumHand(turn) {
+    let sum = 0
+    hasAce = false
+    for(let i=0; i < turn.length; i++) {
+        temp = parseInt(turn[i]['face'].substr(1), 10)
+        if (isNaN(temp)) {
+            if (turn[i]['face'].substr(1) === 'A') {
+                temp = 1;
+                hasAce = true;
+            };
+            if (turn[i]['face'].substr(1) === 'K') temp = 10; 
+            if (turn[i]['face'].substr(1) === 'Q') temp = 10;
+            if (turn[i]['face'].substr(1) === 'J') temp = 10;
+        }
+        sum += temp
+    }
+    if (hasAce) {
+       sum + 10 <= 21 ? sum += 10 : sum += 0;
+        }
+    return sum;
+}
+
+function changeTurn() {
+    turn *= -1
+}
+
+function dealerPlay() {
+    if (sumHand(dealerHand) < 17) {
+        dealerHand.push(deck.shift());
+    } else { stay();
+    }
+}
+
+function checkWinner() {
+    if (sumHand(playerHand) > sumHand(dealerHand)) {
+        winner = PLAYERS
+    }
+} 
+
+init()
